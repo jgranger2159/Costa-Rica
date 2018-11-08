47cb15e487f7c3ff7455643577d59ddc2108ae76
Perry the Platypus <jgranger2159@gmail.com>
2018-11-07 20:54:52 -0700

    added heroku requirements

--- scrape_costa.py
+++ scrape_costa.py
@@ -1,26 +1,14 @@
-from splinter import Browser
 from bs4 import BeautifulSoup as bs
 import time
 import re
 
-
-def init_browser():
-    # @NOTE: Replace the path with your actual path to the chromedriver
-    executable_path = {"executable_path": "chromedriver"}
-    return Browser("chrome", **executable_path, headless=False)
-
-
 def scrape_info():
-    browser = init_browser()
-
     # Visit visitcostarica.herokuapp.com
-    url = "https://visitcostarica.herokuapp.com/"
-    browser.visit(url)
+    html = "https://visitcostarica.herokuapp.com/"
 
     time.sleep(1)
 
     # Scrape page into Soup
-    html = browser.html
     soup = bs(html, "html.parser")
 
     temps = str(soup.find_all("strong"))
@@ -38,9 +26,6 @@ def scrape_info():
         "min_temp": min_temp,
         "max_temp": max_temp
     }
-
-    # Quite the browser after scraping
-    browser.quit()
 
     # Return results
     return costa_data
