bc91c310a676b67dd17762b6a5261e48717a7b1b
Perry the Platypus <jgranger2159@gmail.com>
2018-11-08 11:29:12 -0700

    configured app.py

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scrape_costa
+from dev_config import mongo_uri, flask_debug
 
 # Create an instance of Flask
 app = Flask(__name__)
 
 # Use PyMongo to establish Mongo connection
-mongo = PyMongo(app, uri="mongodb://localhost:27017/weather_app")
+mongo = PyMongo(app, uri=mongo_uri)
 destination_data = []
 
 # Route to render index.html template using data from Mongo
@@ -38,4 +39,4 @@ def scrape():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=flask_debug)
